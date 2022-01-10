edfa21556769aea048ece687f8ae7704a19bd18b
Irina0904 <71022317+Irina0904@users.noreply.github.com>
2022-01-10 15:52:09 +0100

    Bug fixes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var mongoUtil = require('./mongoUtil');
 var mqttClient = require('./mqttHandler');
-var nodemailer = require('nodemailer');
+//var nodemailer = require('nodemailer');
 
 
 const client = mqttClient.getMQTTClient();
@@ -13,20 +13,74 @@ client.on('connect', () => {
   })
   client.on('message', (topic, payload) => {
 
-    if (topic === 'dentistimo/booking-handler') {
-      console.log('Received Message:', topic, payload.toString())
+    mongoUtil.connectToServer(function (err) {
+      if (err) console.log(err);
+      const db = mongoUtil.getDb();
+      const appointments = db.collection("appointments");
 
-      client.publish('dentistimo/booking-request', payload.toString(), { qos: 0, retain: false }, (error) => {
-        if (error) {
-          console.error(error)
+      if (topic === 'dentistimo/new-request') {
+
+        var nRequest = payload.toString();
+        console.log('Received Message:', topic, nRequest);
+
+        client.publish('dentistimo/new-booking-request', nRequest, { qos: 0, retain: false }, (error) => {
+          if (error) {
+            console.error(error)
+          }
+        })
+      } else if (topic === 'dentistimo/send-booking-response') {
+        var bookingResponse = JSON.parse(payload);
+
+        if (bookingResponse.response === 'Approved') {
+
+          let bookingCode = Math.random().toString(36).substring(2);
+          let newRequest = {
+            "clinicName": bookingResponse.bookingRequest.clinicName,
+            "clinicId": bookingResponse.bookingRequest.clinicId,
+            "appointmentDate": bookingResponse.bookingRequest.appointmentDate,
+            //"bookingCode": Math.random().toString(36).substring(2),
+            "firstname": bookingResponse.bookingRequest.firstname,
+            "lastname": bookingResponse.bookingRequest.lastname,
+            "email": bookingResponse.bookingRequest.email,
+            "number": bookingResponse.bookingRequest.number,
+            "description": bookingResponse.bookingRequest.description,
+          }
+
+          appointments.insertOne(newRequest);
+
+          var appointmentDate = new Date(Date.parse(bookingResponse.bookingRequest.appointmentDate))
+          let approveMessage = {
+            "status": "Added",
+            "message": "Your appointment has been made!",
+            "info": "Appointment details have been sent to your email.",
+            "date": appointmentDate.toDateString(),
+            "time": appointmentDate.getHours() + ":" + appointmentDate.getMinutes(),
+            "clinic": bookingResponse.bookingRequest.clinicName
+            //"bookingCode": newRequest.bookingCode
+          }
+
+          console.log(approveMessage);
+
+          client.publish('dentistimo/ui-booking-response', JSON.stringify(approveMessage), { qos: 0, retain: false }, (error) => {
+            if (error) {
+              console.error(error)
+            }
+          })
+        } else {
+          console.log('Rejected');
+
+          let rejectMessage = {
+            "status": "Rejected",
+            "message": "Sorry, this time was not available. Please try another time slot."
+          }
+
+          client.publish('dentistimo/ui-booking-response', JSON.stringify(rejectMessage), { qos: 0, retain: false }, (error) => {
+            if (error) {
+              console.error(error)
+            }
+          })
         }
-      })
-    } else if (topic === "dentistimo/check-appointment") {
-      mongoUtil.connectToServer(function (err) {
-        if (err) console.log(err);
-        const db = mongoUtil.getDb();
-        const appointments = db.collection("appointments");
-
+      } else if (topic === "dentistimo/check-appointment") {
         appointments
           .find({ "bookingCode": payload.toString() })
           .toArray()
@@ -53,106 +107,21 @@ client.on('connect', () => {
               })
             }
           });
-      });
-    } else if (topic === 'dentistimo/booking-response') {
-      var bookingResponse = JSON.parse(payload);
 
-      if (bookingResponse.response === 'Approved') {
-        mongoUtil.connectToServer(function (err) {
-          if (err) console.log(err);
-          const db = mongoUtil.getDb();
-          const appointments = db.collection("appointments");
-
-          let bookingCode = Math.random().toString(36).substring(2);
-          let newRequest = {
-            "clinicName": bookingResponse.bookingRequest.clinicName,
-            "clinicId": bookingResponse.bookingRequest.clinicId,
-            "appointmentDate": bookingResponse.bookingRequest.appointmentDate,
-            "bookingCode": bookingCode,
-            "firstname": bookingResponse.bookingRequest.firstname,
-            "lastname": bookingResponse.bookingRequest.lastname,
-            "email": bookingResponse.bookingRequest.email,
-            "number": bookingResponse.bookingRequest.number,
-            "description": bookingResponse.bookingRequest.description,
-          }
-
-          appointments.insertOne(newRequest);
-
-          var appointmentDate = new Date(Date.parse(bookingResponse.bookingRequest.appointmentDate))
-          let approveMessage = {
-            "status": "Added",
-            "message": "Your appointment has been made!",
-            "info": "Appointment details have been sent to your email.",
-            "date": appointmentDate.toDateString(),
-            "time": appointmentDate.getHours() + ":" + appointmentDate.getMinutes(),
-            "clinic": bookingResponse.bookingRequest.clinicName,
-            "bookingCode": bookingCode
-          }
-
-          console.log(approveMessage);
-
-          var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: 'dentistimoclinics@gmail.com',
-              pass: 'dentistimo123'
-            }
-          });
-
-          var mailOptions = {
-            from: 'dentistimoclinics@gmail.com',
-            to: 'turquis.amanda@gmail.com',
-            subject: 'Booking Confirmation',
-            text: "Your appointment has been made!" + " \n " + " \n "
-              + "Clinic: " + bookingResponse.bookingRequest.clinicName + " \n "
-              + "Date: " + appointmentDate.toDateString() + " \n "
-              + "Time: " + appointmentDate.getHours() + ":" + appointmentDate.getMinutes() + " \n "
-              + "Booking code: " + bookingCode
-          };
-
-          transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-
-          client.publish('dentistimo/ui-booking-response', JSON.stringify(approveMessage), { qos: 0, retain: false }, (error) => {
-            if (error) {
-              console.error(error)
-            }
-          })
-        });
-      } else {
-        console.log('Rejected');
-
-        let rejectMessage = {
-          "status": "Rejected",
-          "message": "Sorry, this time was not available. Please try another time slot."
-        }
-
-        client.publish('dentistimo/ui-booking-response', JSON.stringify(rejectMessage), { qos: 0, retain: false }, (error) => {
-          if (error) {
-            console.error(error)
+      } else if (topic === 'dentistimo/bookingCode') {
+        console.log('Booking Code received for deletion')
+        var receivedBC = JSON.parse(payload);
+        console.log('Received Booking Code:', receivedBC)
+  
+        appointments.findOneAndDelete({ bookingCode: receivedBC }, function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully deleted the appointment")
           }
         })
       }
-    } else if (topic === 'dentistimo/bookingCode') {
-      console.log('Booking Code received for deletion')
-      var receivedBC = JSON.parse(payload);
-      console.log('Received Booking Code:', receivedBC)
-
-      const db = mongoUtil.getDb();
-      const appointments = db.collection("appointments");
-
-      appointments.findOneAndDelete({ bookingCode: receivedBC }, function (err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Successfully deleted the appointment")
-        }
-      })
-    }
+  
+    })
   })
 })
